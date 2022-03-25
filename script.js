f29e4a61dd511af510528f243f8aa71b7c8d4b10
Daria Smirnova <darya.smirn1@yandex.ru>
2022-03-25 14:33:54 +0300

    Выполнила дз №2

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = "Project name";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 15000;
+const rollback = 5;
+const fullPrice = 50000;
+const adaptive = true;
 
-alert("Привет из курса по JS от Glo Academy");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log("Hello from JS course");
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей');
+
+console.log(screens.toLowerCase().split(', '));
+
+console.log('Процент отката посреднику за работу ' + (fullPrice * rollback / 100));
