131c494545219e4fcb2ba68e56255df8b486f440
Daria Smirnova <darya.smirn1@yandex.ru>
2022-03-28 17:08:39 +0300

    Выполнила дз №3

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const title = "Project name";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 15000;
+'use strict';
+
+const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?');
+const screenPrice = parseInt(prompt('Сколько будет стоить данная работа?'));
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice1 = parseInt(prompt('Сколько это будет стоить?'));
+const service2 = prompt('Какой дополнительный тип услуги нужен?');
+const servicePrice2 = parseInt(prompt('Сколько это будет стоить?'));
+
 const rollback = 5;
-const fullPrice = 50000;
-const adaptive = true;
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+const servicePercentPrice = fullPrice - Math.floor(fullPrice * rollback / 100);
+console.log(servicePercentPrice);
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -17,3 +27,18 @@ console.log('Стоимость разработки сайта ' + fullPrice + 
 console.log(screens.toLowerCase().split(', '));
 
 console.log('Процент отката посреднику за работу ' + (fullPrice * rollback / 100));
+
+switch (true) {
+	case fullPrice >= 30000:
+		console.log('Даем скидку в 10%');
+		break;
+	case fullPrice >= 15000 && fullPrice < 30000:
+		console.log('Даем скидку в 5%');
+		break;
+	case fullPrice < 15000 && fullPrice >= 0:
+		console.log('Скидка не предусмотрена');
+		break;
+	case fullPrice < 0:
+		console.log('Что-то пошло не так');
+		break;		
+}
